e9b555146c70cfef3a8d5298519f325001275c5a
Abby X Su <77119427+AbbyXSu@users.noreply.github.com>
2021-03-23 23:33:31 +0000

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -20,13 +20,13 @@ module.exports = function (config) {
       fixWebpackSourcePaths: true
     },
     
-    #reporters: ['progress', 'kjhtml'],
-    #port: 9876,
-    #colors: true,
-    #logLevel: config.LOG_INFO,
-    #autoWatch: true,
-    #browsers: ['Chrome'],
-    #singleRun: false
+   //reporters: ['progress', 'kjhtml'],
+    //port: 9876,
+    //colors: true,
+    //logLevel: config.LOG_INFO,
+    //autoWatch: true,
+    //browsers: ['Chrome'],
+    //singleRun: false
     singleRun: true,
     reporters: ['dots', 'junit'],
     junitReporter: {
